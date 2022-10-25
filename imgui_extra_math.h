4cd2e60bc634bd30067157cb4211399a96ae49a0
iPhantomiKX <cmpsun98@gmail.com>
2022-10-25 14:15:47 +0800

    Changed #include<imgui.h> and #include<imgui_internal.h> to #include "imgui.h" and #include"imgui_internal.h"

--- imgui_extra_math.h
+++ imgui_extra_math.h
@@ -15,11 +15,11 @@
 
 
 //------------------------------------------------------------------------------
-# include <imgui.h>
+# include "imgui.h"
 # ifndef IMGUI_DEFINE_MATH_OPERATORS
 #     define IMGUI_DEFINE_MATH_OPERATORS
 # endif
-# include <imgui_internal.h>
+# include "imgui_internal.h"
 
 
 //------------------------------------------------------------------------------
